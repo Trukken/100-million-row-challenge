c4c0fd0e55a6dd899eed27ea848a3925410d8dee
Szabolcs Tomolya <tomolya12@gmail.com>
2026-02-25 15:14:36 +0100

    Added: - Multithreaded parser - Optimizations

--- app/Parser.php
+++ app/Parser.php
@@ -4,23 +4,36 @@ namespace App;
 
 final class Parser
 {
-    private const string DELIMITER = ',';
     private const int STICHER_PREFIX_LENGTH = 19;
     private const int DATE_LENGTH = 10;
+    private const int COMMA_DELIMITER_POSITION = 26;
+    private const int NUM_PROCESSES = 6;
+    private const int CHUNK_SIZE = 2_048_576;
 
 
     public function parse(string $inputPath, string $outputPath): void
     {
-        $numProcesses = 4;
+        $numProcesses = self::NUM_PROCESSES;
         $fileSize = filesize($inputPath);
         $chunkSize = ceil($fileSize / $numProcesses);
+
+        $boundaries = [0];
+        $handle = fopen($inputPath, 'rb');
+        for ($i = 1; $i < $numProcesses; $i++) {
+            fseek($handle, $i * $chunkSize);
+            fgets($handle);
+            $boundaries[] = ftell($handle);
+        }
+        fclose($handle);
+        $boundaries[] = $fileSize;
+        
         $tempFiles = [];
         $pids = [];
 
         for ($i = 0; $i < $numProcesses; $i++) {
             $tempFiles[$i] = tempnam(sys_get_temp_dir(), "parser_chunk_$i");
-            $startByte = $i * $chunkSize;
-            $endByte = min(($i + 1) * $chunkSize - 1, $fileSize - 1);
+            $startByte = $boundaries[$i];
+            $endByte = $boundaries[$i + 1] - 1;
 
             $pid = pcntl_fork();
             if ($pid == -1) {
@@ -39,7 +52,7 @@ final class Parser
 
         $existingPath = [];
         for ($i = 0; $i < $numProcesses; $i++) {
-            $chunkResults = unserialize(file_get_contents($tempFiles[$i]));
+            $chunkResults = igbinary_unserialize(file_get_contents($tempFiles[$i]));
             unlink($tempFiles[$i]);
 
             foreach ($chunkResults as $path => $dates) {
@@ -58,34 +71,56 @@ final class Parser
 
     public static function processChunk(string $inputPath, int $startByte, int $endByte, string $outputFile): void
     {
-        $file = fopen($inputPath, 'r');
-        $existingPath = [];
-
+        $file = fopen($inputPath, 'rb');
+        stream_set_read_buffer($file, 0);
         fseek($file, $startByte);
 
-        if ($startByte > 0) {
-            fgets($file);
+        $toRead = $endByte - $startByte;
+        $remainder = '';
+        $existingPath = [];
+
+        while ($toRead > 0) {
+            $chunk = fread($file, $toRead > self::CHUNK_SIZE ? self::CHUNK_SIZE : $toRead);
+            $toRead -= strlen($chunk);
+
+            if ($remainder !== '') {
+                $chunk = $remainder . $chunk;
+            }
+
+            $newLine = strrpos($chunk, "\n");
+            $remainder = substr($chunk, $newLine + 1);
+
+            $pos = 0;
+            while ($pos < $newLine) {
+                $newLinePos = strpos($chunk, "\n", $pos);
+                $line = substr($chunk, $pos, $newLinePos - $pos);
+                
+                $content = substr($line, self::STICHER_PREFIX_LENGTH);
+                $path = substr($content, 0, - self::COMMA_DELIMITER_POSITION);
+
+                $date = substr($content, - self::COMMA_DELIMITER_POSITION + 1, self::DATE_LENGTH);
+                if (strlen($date) !== self::DATE_LENGTH) {
+                    $pos = $newLinePos + 1;
+                    continue;
+                }
+
+                $existingPath[$path][$date] = ($existingPath[$path][$date] ?? 0) + 1;
+                $pos = $newLinePos + 1;
+            }
         }
 
-        while (ftell($file) <= $endByte && ($line = fgets($file)) !== false) {
-            $content = substr($line, self::STICHER_PREFIX_LENGTH);
-            $commaPos = strpos($content, self::DELIMITER);
-            if ($commaPos === false) {
-                continue;
+        if ($remainder !== '') {
+            $content = substr($remainder, self::STICHER_PREFIX_LENGTH);
+            $path = substr($content, 0, - self::COMMA_DELIMITER_POSITION);
+
+            $date = substr($content, - self::COMMA_DELIMITER_POSITION + 1, self::DATE_LENGTH);
+            if (strlen($date) === self::DATE_LENGTH) {
+                $existingPath[$path][$date] = ($existingPath[$path][$date] ?? 0) + 1;
             }
-
-            $path = substr($content, 0, $commaPos);
-
-            $date = substr($content, $commaPos + 1, self::DATE_LENGTH);
-            if (strlen($date) !== self::DATE_LENGTH) {
-                continue;
-            }
-
-            $existingPath[$path][$date] = ($existingPath[$path][$date] ?? 0) + 1;
         }
 
         fclose($file);
 
-        file_put_contents($outputFile, serialize($existingPath));
+        file_put_contents($outputFile, igbinary_serialize($existingPath));
     }
 }
